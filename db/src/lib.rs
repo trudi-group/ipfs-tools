f4f0bb60e6d5edc239779d06b3a50159a5bccb50
Leo Balduf <leobalduf@gmail.com>
2020-05-19 12:13:40 +0200

    db: add connection pooling

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -6,6 +6,7 @@ extern crate diesel;
 extern crate lazy_static;
 
 use cid::Cid;
+use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
 use diesel::{Connection, PgConnection};
 use failure::ResultExt;
 use ipfs_resolver_common::Result;
@@ -15,6 +16,26 @@ use std::env;
 pub mod db;
 pub mod model;
 pub mod schema;
+
+pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+pub type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
+
+fn init_pool(database_url: &str) -> std::result::Result<PgPool, PoolError> {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    Pool::builder()
+        .min_idle(Some(10))
+        .max_size(64)
+        .build(manager)
+}
+
+pub fn create_pool() -> Result<PgPool> {
+    dotenv::dotenv().ok();
+    let database_url = env::var("DATABASE_URL").context("DATABASE_URL must be set")?;
+
+    let pool = init_pool(&database_url)?;
+
+    Ok(pool)
+}
 
 pub fn establish_connection() -> Result<PgConnection> {
     dotenv::dotenv().ok();
